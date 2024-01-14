3a91a4d41829745cab419940323ca4995ee2f210
manuel-spec <amaniase084@gmail.com>
2024-01-14 10:22:23 +0300

    use state

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Home = () => {
+    const [text, setText] = useState('')
 
     const handleClick = () => {
-        console.log("hey")
+        setText("Bitch don't Touch me")
     }
     return (
-        <div className='flex justify-center'>
+        <div className='flex flex-col justify-center '>
             <h1 className='text-3xl'>Home</h1>
+            <h1 className='p-4 text-red-500 text-4xl'>{text}</h1>
             <button className='border py-3 px-2' onClick={handleClick}>click me</button>
         </div>
     )
