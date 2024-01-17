11bda2f357ac60dbf2d25f9aa89edb6b01a5cb0c
manuel-spec <amaniase084@gmail.com>
2024-01-17 10:42:32 +0300

    react router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import About from './Pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Home</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
